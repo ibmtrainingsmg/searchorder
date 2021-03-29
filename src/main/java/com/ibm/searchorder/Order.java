f64099b417ede93053ee9e3c65c1babc5dcf3922
unknown <ibmjavanh17@iiht.tech>
2021-03-29 12:54:32 +0530

    updated code with mongodb connection

---
+++ src/main/java/com/ibm/searchorder/Order.java
@@ -0,0 +1,43 @@
+package com.ibm.searchorder;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.Id;
+
+public class Order {
+	@NotNull
+	@NotBlank
+	private String item;
+	@Id
+	private String id;
+	private float price;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		if(price<=0) {
+			throw new IllegalArgumentException("price cannot be negative");
+		}
+		this.price = price;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+	
+}
---
+++ src/main/java/com/ibm/searchorder/OrderService.java
@@ -0,0 +1,22 @@
+package com.ibm.searchorder;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+	@Autowired
+	OrderRepository orderRepository;
+
+	public List<Order> getOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Optional<Order> getOrder(String orderId) {
+		return orderRepository.findById(orderId);
+	}
+
+}
